36f206aba41e61b18e0233c23be26475114e6278
Mahmoud-T-Almetwally <mahmoudtariq@std.mans.edu.eg>
2024-08-03 16:19:55 +0300

    Changed Folder name and moved attention.py

--- sd/decoder.py
+++ sd/decoder.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
-from attention import SelfAttention
+from stable_diffusion_Pytorch.attention import SelfAttention
 
 class VAE_ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
